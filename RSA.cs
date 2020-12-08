f29791c1f946a2eb41d413edd2911c1cce9d9b69
SiAvMaAnR <samarkin200220022002@gmail.com>
2020-12-08 03:14:07 +0300

    Добавьте файлы проекта.

---
+++ RSA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	private uint p = 36563;
+	private uint q = 57731;
+
+	public Class1(uint p, uint q)
+	{
+
+	}
+
+	private void keyGeneration()
+	{
+
+	}
+
+}
